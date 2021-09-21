4c8cff871f93e50ef633e4d391c26a716f6dee3b
Rashad Bowman <RashadCBowman@yahoo.com>
2021-09-21 11:04:05 -0400

    Week 2 revised

--- projectOnboardingCode/src/main/java/com/revature/week2/MemorySize.java
+++ projectOnboardingCode/src/main/java/com/revature/week2/MemorySize.java
@@ -2,26 +2,26 @@ package com.revature.week2;
 
 public class MemorySize {
 
-	public String actualMemorySize(String size) {
-		//to catch a NumberFormatException if presented
+	public String actualMemorySize(String memorySize) {
+		// to catch a NumberFormatException if presented
 		try {
-			//checks if the size is correct, it should be higher then 2 due to number and byte type
-			if (size.length() > 2) {
-				// creates String Builder
-				StringBuilder sb = new StringBuilder();
+			// checks if the size is correct, it should be higher then 2 due to number and
+			// byte type
+			if (memorySize.length() > 2) {
+				StringBuilder sb = new StringBuilder(); // creates String Builder
 				// Injects GB or MB
-				sb.append(size.substring(size.length() - 2, size.length()));
-				//checks if GB or MB is presented
+				sb.append(memorySize.substring(memorySize.length() - 2, memorySize.length()));
+				// checks if GB or MB is presented
 				if (sb.toString().contains("GB") || sb.toString().contains("MB")) {
 					// Injects number excluding last 2 due to being either GB or MB
-					Float num = Float.parseFloat(size.substring(0, size.length() - 2));
+					Float num = Float.parseFloat(memorySize.substring(0, memorySize.length() - 2));
 					// Calculates the memory loss by 7% (0.07)
 					num = num - (num * 0.07f);
 					// If the number is lower then 1, convert it into a MB
 					if (num < 1 && sb.toString().equals("GB")) {
-						//pushes 3 numbers above the decimal for MB
+						// pushes 3 numbers above the decimal for MB
 						num = num * 1000;
-						//replaces GB with MB
+						// replaces GB with MB
 						sb.replace(0, sb.length(), "MB");
 					}
 					// rounds the number depending on MB or GB
@@ -35,7 +35,7 @@ public class MemorySize {
 		} catch (NumberFormatException e) {
 			System.err.println(e);
 		}
-		//returns information if not presented correctly
-		return size + " is incorrect information!";
+		// returns information if not presented correctly
+		return memorySize + " is incorrect information!";
 	}
 }
